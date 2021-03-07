136882aa27fa89f8dc8ec2467fb6518018823b03
therainmaiker <57842299+therainmaiker@users.noreply.github.com>
2021-03-07 18:56:17 +0100

    student context added + Students Controller added

---
+++ source/repos/CrudUsingEntity/CrudUsingEntity/Data/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CrudUsingEntity.Models;
+
+namespace CrudUsingEntity.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext (DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
---
+++ source/repos/CrudUsingEntity/CrudUsingEntity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudUsingEntity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public string Address { get; set; }
+
+
+         public virtual ICollection<Student> Students { get; set; }
+    }
+}
